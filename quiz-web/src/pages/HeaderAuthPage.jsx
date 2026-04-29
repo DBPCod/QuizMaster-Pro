a9e59a6383604af5d90b5a62311eea4a3bc6d802
DBPCod <dbp0932@gmail.com>
2026-04-29 13:34:42 +0000

    fix(HeaderAuthPage): sửa lại tên function

--- quiz-web/src/pages/HeaderAuthPage.jsx
+++ quiz-web/src/pages/HeaderAuthPage.jsx
@@ -1,6 +1,6 @@
 import { FaPuzzlePiece } from 'react-icons/fa';
 
-export default function HeaderAuth() {
+export default function HeaderAuthPage() {
   return (
     <section className="flex w-full bg-[#E8ECFB] justify-between items-center px-6 h-12">
       <div className="flex items-center gap-2">
