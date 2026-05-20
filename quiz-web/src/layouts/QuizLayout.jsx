d2e47dd083f751c507b5aad725e7c4a58f8389ad
dbp <dbp0932@gmail.com>
2026-05-20 22:09:49 +0700

    feat(HeaderMain): thêm component header

--- quiz-web/src/layouts/QuizLayout.jsx
+++ quiz-web/src/layouts/QuizLayout.jsx
@@ -1,13 +1,13 @@
 import HeaderAuth from "../pages/HeaderAuthPage";
 import { Outlet } from 'react-router-dom';
 import { FaCopyright } from 'react-icons/fa';
-import HeaderAuthPage from "../pages/HeaderAuthPage";
+import HeaderMain from "../components/HeaderMain";
 const QuizLayout = () =>
 {
   return (
       <div className="flex flex-col h-screen overflow-hidden">
         <div className="flex-shrink-0">
-          <HeaderAuthPage />
+          <HeaderMain />
         </div>
         <main className="flex-grow w-full overflow-hidden">
           <Outlet></Outlet>
