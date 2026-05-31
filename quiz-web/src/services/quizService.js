0ce121de6c59fc5b64b82c4fd07b65fce2907780
dbp <dbp0932@gmail.com>
2026-05-31 14:38:34 +0700

    feat(quiz): thêm chức năng sửa quiz

--- quiz-web/src/services/quizService.js
+++ quiz-web/src/services/quizService.js
@@ -22,7 +22,17 @@ const quizService = {
     delete: async (id) => {
         const response = await axiosClient.delete(`quizzes/${id}`);
         return response.data;
-    }
+    },
+    update: async (id, quizData) => {
+        const response = await axiosClient.put(`/quizzes/${id}`, quizData);
+        return response.data;
+    },
+    
+    // Thêm hàm lấy chi tiết 1 bài quiz từ Backend thực tế
+    getById: async (id) => {
+        const response = await axiosClient.get(`/quizzes/${id}`);
+        return response.data;
+    },
 };
 
 export default quizService;
